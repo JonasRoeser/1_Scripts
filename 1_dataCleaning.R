5d96bed350f7e474066edd4d0a87fa4442231f77
leonidasbothmer <44315917+leonidasbothmer@users.noreply.github.com>
2018-11-13 16:07:59 +0100

    Columns description

---
+++ 1_dataCleaning.R
@@ -0,0 +1,32 @@
+# Was für features wollen wir?
+#   
+# Vorhanden
+# Weltranglistenrang zum Zeitpunkt des Spiels (W/L)
+# Weltranglistenpoints (W/L)
+# Untergrund
+# Outdoor/Indoor
+# Location (Stadt)
+# Tournament
+# Date
+# Series
+# Round
+# Best of Three/Five
+# Name (W/L)
+# Spiele in jedem Satz (W/L)
+# Losersets
+# Quoten
+# Age
+
+# 1. Wie brauchen ein Dataset mit allen Spielern und Ihren Geburtsdaten
+# 2. Zwei Spalten erstellen, die wir mit ALter füllen wollen (W/L)
+# 3. Aus dem Dataset mit Playerdata Geburstdatum nehmen und mit Spieldatum zu einem Alter
+#    errechnen (Rechnen mit Dates siehe Script aus Workshop)
+# 4. 
+# 
+# Titel gewonnen:
+# Spiele gewonnnen
+# 
+
+
+
+
