38e61327c0f37ccbffd6182fc7444c65b228a6a5
leonidasbothmer <44315917+leonidasbothmer@users.noreply.github.com>
2018-11-15 14:05:06 +0100

    Cleaned and saved to Data

--- 01_dataCleaning_1.R
+++ 01_dataCleaning_1.R
@@ -58,63 +58,61 @@ playerOverviews = read.csv("../Roeser, Jonas - 2_Data/Downloaded Potential Data/
 rankings1973_2017 = read.csv("../Roeser, Jonas - 2_Data/Downloaded Potential Data/rankings_1973-2017_csv.csv")
 tournaments1877_2017 = read.csv("../Roeser, Jonas - 2_Data/Downloaded Potential Data/tournaments_1877-2017_unindexed_csv.csv")
 
+
+# Cut rankings1973_2017 ----------------------------------------------
+
+typeof(rankings1973_2017$week_year)
+rankings <- rankings1973_2017 %>%
+  filter(week_year >= 1991)
+# Check earliest date
+min(rankings$week_year)
+# Check if complete
+nrow(is.na(rankings$week_year))
+
+# Cut tournaments1877_2017 -------------------------------------------
+
+typeof(tournaments1877_2017$tourney_year)
+tournaments <- tournaments1877_2017 %>%
+  filter(tourney_year >= 1991)
+# Check earliest date
+min(tournaments$tourney_year)
+# Check if complete
+nrow(is.na(tournaments$tourney_year))
+
 # Stats Check --------------------------------------------------------
 
-<<<<<<< HEAD
 # Checking if colnames of matchStats match
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Cutting rankings1973_2017 -----------------------------------------------
-typeof(rankings1973_2017$week_year)
-rankings1991_2017 <- rankings1973_2017 %>%
-  filter(week_year >= 1991)
-# checking
-min(rankings1991_2017$week_year)
-# complete?
-nrow(is.na(rankings1991_2017$week_year))
-
-
-# Cutting tournaments1877_2017 --------------------------------------------
-typeof(tournaments1877_2017$tourney_year)
-tournaments1991_2017 <- tournaments1877_2017 %>%
-  filter(tourney_year >= 1991)
-# checking
-min(tournaments1991_2017$tourney_year)
-# complete?
-nrow(is.na(tournaments1991_2017$tourney_year))
-=======
-=======
->>>>>>> 54e30177e8d463d130cd3b64aa0bb009e9f00996
-# Stats check ------
->>>>>>> 873f4aec42fdc72d8b763ee1f21e189440b6711a
-colnames(matchStats1991_2016) == colnames(matchStats2017)              # All true
+colnames(matchStats1991_2016) == colnames(matchStats2017)               # All true
 # Checking if classes of matchStats columns match
-sapply(matchStats1991_2016, class) == sapply(matchStats2017, class)    # All true
+sapply(matchStats1991_2016, class) == sapply(matchStats2017, class)     # All true
 # Checking if types of matchStats columns match
-sapply(matchStats1991_2016, typeof) == sapply(matchStats2017, typeof)  # All true
+sapply(matchStats1991_2016, typeof) == sapply(matchStats2017, typeof)   # All true
 
 # Combining matchStats
-matchStats1991_2017 = rbind(matchStats1991_2016, matchStats2017)
+matchStats = rbind(matchStats1991_2016, matchStats2017)
 
-<<<<<<< HEAD
 # Scores Check -------------------------------------------------------
-=======
-<<<<<<< HEAD
->>>>>>> 54e30177e8d463d130cd3b64aa0bb009e9f00996
-=======
->>>>>>> 54e30177e8d463d130cd3b64aa0bb009e9f00996
->>>>>>> 873f4aec42fdc72d8b763ee1f21e189440b6711a
 
 # Checking if colnames of matchScores match
-colnames(matchScores1991_2016) == colnames(matchScores2017)
+colnames(matchScores1991_2016) == colnames(matchScores2017)             # All true
 # Checking if classes of matchScores columns match
-sapply(matchScores1991_2016, class) == sapply(matchScores2017, class)
+sapply(matchScores1991_2016, class) == sapply(matchScores2017, class)   # All true
 # Checking if types of matchScores columns match
-sapply(matchScores1991_2016, typeof) == sapply(matchScores2017, typeof)
+sapply(matchScores1991_2016, typeof) == sapply(matchScores2017, typeof) # All true
 
 # Combining matchScores
-matchScores1991_2017 = rbind(matchScores1991_2016, matchScores2017)
+matchScores = rbind(matchScores1991_2016, matchScores2017)
 
-# Removing other datasets
-rm(matchStats1991_2016, matchStats2017, matchScores1991_2016, matchScores2017)
+# Removing other datasets --------------------------------------------
+rm(matchStats1991_2016, 
+   matchStats2017, 
+   matchScores1991_2016, 
+   matchScores2017,
+   rankings1973_2017,
+   tournaments1877_2017)
+
+# Save ---------------------------------------------------------------
+
+save.image(file="../Roeser, Jonas - 2_Data/data1991_2017.RData")
+
+
---
+++ 01_dataCleaning_2.R
@@ -0,0 +1,3 @@
+rm(list = ls())
+load("../Roeser, Jonas - 2_Data/data1991_2017.RData")
+load("../2_Data/data1991_2017.RData")
