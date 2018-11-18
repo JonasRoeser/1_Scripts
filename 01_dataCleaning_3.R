83aa71464fb9ff90732a6bdc2be2c9cfa1195af3
JonasRoeser <jonas@schneider-roeser.de>
2018-11-18 22:07:55 +0100

    Building model

--- 01_dataCleaning_3.R
+++ 01_dataCleaning_3.R
@@ -1,1 +1,14 @@
 load("D.RData")
+D = D %>%
+  mutate(Y = 1)
+Xtrain = as.matrix(D[1:15000, 5:6])
+Ytrain = as.matrix(D[1:15000, 7])
+Xtest= as.matrix(D[15001:22000, 5:6])
+Ytest = as.matrix(D[15001:22000, 7])
+
+data <- data.frame(Ytrain, Xtrain)
+model <- glm(Ytrain ~ ., data, family=binomial(link='logit'))
+
+beta_logistic  = model$coefficients
+
+cbind(1,Xtest) %*% beta_logistic 
