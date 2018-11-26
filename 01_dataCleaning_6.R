4377ee9b97f6b45b2dc84e2ac83ef2b5ea51f10f
JonasRoeser <jonas@schneider-roeser.de>
2018-11-27 00:25:51 +0100

    Form & Condition Wins Features

--- 01_dataCleaning_6.R
+++ 01_dataCleaning_6.R
@@ -1,5 +1,5 @@
 
-# CREATING NEW FEATURES FOR D
+# CREATING FORM FEATURE FOR D
 
 library(tidyverse)
 
@@ -14,51 +14,72 @@ load("../2_Data/D1.RData")
 
 # Creating form feature ---------------------------------------------------
 
-D1 = D1[c(1:4,19,37,55)] %>%
+D1 = D1[c(58,2,20,38,56)] %>%
   mutate(form_player0 = NA) %>%
   mutate(form_player1 = NA)
 
-D1 = D1%>%
-  mutate(mcFreezy = paste(D1$player0, D1$player1, D1$player0, sep = "-"))
-
-for (i in 92000:nrow(D1)) {
-  matches1 = which(as.character(D1$player1) == as.character(D1$player1[i]))
-  matches0 = which(as.character(D1$player0) == as.character(D1$player1[i]))
-  matches_smaller_i_indeces1 = which(matches0<i)
-  matches_smaller_i_indeces0 = which(matches0<i)
-  matches_smaller_i1 = matches1[matches_smaller_i_indeces1]
-  matches_smaller_i0 = matches0[matches_smaller_i_indeces0]
-  matches_smaller_iboth = sort(append(matches_smaller_i1, matches_smaller_i0))
-  
-  n_matches = length(matches_smaller_iboth)
-  if (n_matches >= 10) {
-    sth = D1$Y[matches_smaller_iboth[c((n_matches)-9):n_matches]]
-    bruv0 = match(matches_smaller_i1[c((n_matches)-9):n_matches], matches_smaller_iboth[c((n_matches)-9):n_matches])
-    sth0 = bruv0[which(!is.na(bruv0))]
-    bruv1 = match(matches_smaller_i1[c((n_matches)-9):n_matches], matches_smaller_iboth[c((n_matches)-9):n_matches])
-    sth1 = bruv1[which(!is.na(bruv1))]
-    D1$form_player1[i] = length(which(sth[sth0] == 0)) + length(which(sth[sth1] == 1))
-  } else {
-    D1$form_player1[i] = NA
-  }
-  
-  matches1 = which(as.character(D1$player1) == as.character(D1$player0[i]))
+for (i in 1:nrow(D1)) {
+  # Calculating Form for player0
   matches0 = which(as.character(D1$player0) == as.character(D1$player0[i]))
-  matches_smaller_i_indeces1 = which(matches0<i)
+  matches1 = which(as.character(D1$player1) == as.character(D1$player0[i]))
   matches_smaller_i_indeces0 = which(matches0<i)
-  matches_smaller_i1 = matches1[matches_smaller_i_indeces1]
+  matches_smaller_i_indeces1 = which(matches1<i)
   matches_smaller_i0 = matches0[matches_smaller_i_indeces0]
+  matches_smaller_i1 = matches1[matches_smaller_i_indeces1]
   matches_smaller_iboth = sort(append(matches_smaller_i1, matches_smaller_i0))
   
+  n_matches0 = length(matches_smaller_i0)
+  n_matches1 = length(matches_smaller_i1)
   n_matches = length(matches_smaller_iboth)
   if (n_matches >= 10) {
-    sth = D1$Y[matches_smaller_iboth[c((n_matches)-9):n_matches]]
-    bruv0 = match(matches_smaller_i1[c((n_matches)-9):n_matches], matches_smaller_iboth[c((n_matches)-9):n_matches])
-    sth0 = bruv0[which(!is.na(bruv0))]
-    bruv1 = match(matches_smaller_i1[c((n_matches)-9):n_matches], matches_smaller_iboth[c((n_matches)-9):n_matches])
-    sth1 = bruv1[which(!is.na(bruv1))]
-    D1$form_player0[i] = length(which(sth[sth0] == 0)) + length(which(sth[sth1] == 1))
+    matchResults = D1$Y[matches_smaller_iboth[c((n_matches)-9):n_matches]]
+    
+    matchesInPlayer0Column = match(matches_smaller_i0[c((n_matches0-9):n_matches0)[which(c((n_matches0-9):n_matches0)>0)]],
+                                   matches_smaller_iboth[c((n_matches)-9):n_matches])
+    matchesInPlayer0ColumnNoNA = matchesInPlayer0Column[which(!is.na(matchesInPlayer0Column))]
+    
+    matchesInPlayer1Column = match(matches_smaller_i1[c((n_matches1-9):n_matches1)[which(c((n_matches1-9):n_matches1)>0)]],
+                                   matches_smaller_iboth[c((n_matches)-9):n_matches])
+    matchesInPlayer1ColumnNoNA = matchesInPlayer1Column[which(!is.na(matchesInPlayer1Column))]
+    
+    D1$form_player0[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
   } else {
     D1$form_player0[i] = NA
   }
+  
+  # Calculating form for player1
+  matches0 = which(as.character(D1$player0) == as.character(D1$player1[i]))
+  matches1 = which(as.character(D1$player1) == as.character(D1$player1[i]))
+  matches_smaller_i_indeces0 = which(matches0<i)
+  matches_smaller_i_indeces1 = which(matches1<i)
+  matches_smaller_i0 = matches0[matches_smaller_i_indeces0]
+  matches_smaller_i1 = matches1[matches_smaller_i_indeces1]
+  matches_smaller_iboth = sort(append(matches_smaller_i1, matches_smaller_i0))
+  
+  n_matches0 = length(matches_smaller_i0)
+  n_matches1 = length(matches_smaller_i1)
+  n_matches = length(matches_smaller_iboth)
+  if (n_matches >= 10) {
+    matchResults = D1$Y[matches_smaller_iboth[c((n_matches)-9):n_matches]]
+    
+    matchesInPlayer0Column = match(matches_smaller_i0[c((n_matches0-9):n_matches0)[which(c((n_matches0-9):n_matches0)>0)]],
+                                   matches_smaller_iboth[c((n_matches)-9):n_matches])
+    matchesInPlayer0ColumnNoNA = matchesInPlayer0Column[which(!is.na(matchesInPlayer0Column))]
+    
+    matchesInPlayer1Column = match(matches_smaller_i1[c((n_matches1-9):n_matches1)[which(c((n_matches1-9):n_matches1)>0)]],
+                                   matches_smaller_iboth[c((n_matches)-9):n_matches])
+    matchesInPlayer1ColumnNoNA = matchesInPlayer1Column[which(!is.na(matchesInPlayer1Column))]
+    
+    D1$form_player1[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
+  } else {
+    D1$form_player1[i] = NA
+  }
 }
+
+
+# Saving -------------------------------------------------------------
+
+D2 = D1
+# save(D2, file = "../Roeser, Jonas - 2_Data/D2.RData")
+D2noNA = D1[which(!is.na(D1$form_player1) & !is.na(D1$form_player0)),]
+# save(D2noNA, file = "../Roeser, Jonas - 2_Data/D2noNA.RData")
