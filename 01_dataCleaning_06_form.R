7e33ca5152feca263abb35713d9cdd1208f9ec43
JonasRoeser <jonas@schneider-roeser.de>
2018-11-30 15:06:21 +0100

    Update 01_dataCleaning_06_form.R

--- 01_dataCleaning_06_form.R
+++ 01_dataCleaning_06_form.R
@@ -12,11 +12,11 @@ load("../Roeser, Jonas - 2_Data/D1.RData")
 load("../2_Data/D1.RData")
 
 
-# Creating form feature ---------------------------------------------------
+# Creating form feature for last 10 games ---------------------------------------------------
 
-D1 = D1[c(58,2,20,38,56)] %>%
-  mutate(form_player0 = NA) %>%
-  mutate(form_player1 = NA)
+D1 = D1[c(5000:6000),c(58,2,20,38,56)] %>%
+  mutate(form_10_player0 = NA) %>%
+  mutate(form_10_player1 = NA)
 
 for (i in 1:nrow(D1)) {
   # Calculating Form for player0
@@ -42,9 +42,9 @@ for (i in 1:nrow(D1)) {
                                    matches_smaller_iboth[c((n_matches)-9):n_matches])
     matchesInPlayer1ColumnNoNA = matchesInPlayer1Column[which(!is.na(matchesInPlayer1Column))]
     
-    D1$form_player0[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
+    D1$form_10_player0[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
   } else {
-    D1$form_player0[i] = NA
+    D1$form_10_player0[i] = NA
   }
   
   # Calculating form for player1
@@ -70,14 +70,150 @@ for (i in 1:nrow(D1)) {
                                    matches_smaller_iboth[c((n_matches)-9):n_matches])
     matchesInPlayer1ColumnNoNA = matchesInPlayer1Column[which(!is.na(matchesInPlayer1Column))]
     
-    D1$form_player1[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
+    D1$form_10_player1[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
   } else {
-    D1$form_player1[i] = NA
+    D1$form_10_player1[i] = NA
   }
 }
+
+
+# Creating form feature for last 10 games ---------------------------------------------------
+
+D1 = D1 %>%
+  mutate(form_5_player0 = NA) %>%
+  mutate(form_5_player1 = NA)
+
+for (i in 1:nrow(D1)) {
+  # Calculating Form for player0
+  matches0 = which(as.character(D1$player0) == as.character(D1$player0[i]))
+  matches1 = which(as.character(D1$player1) == as.character(D1$player0[i]))
+  matches_smaller_i_indeces0 = which(matches0<i)
+  matches_smaller_i_indeces1 = which(matches1<i)
+  matches_smaller_i0 = matches0[matches_smaller_i_indeces0]
+  matches_smaller_i1 = matches1[matches_smaller_i_indeces1]
+  matches_smaller_iboth = sort(append(matches_smaller_i1, matches_smaller_i0))
+  
+  n_matches0 = length(matches_smaller_i0)
+  n_matches1 = length(matches_smaller_i1)
+  n_matches = length(matches_smaller_iboth)
+  if (n_matches >= 5) {
+    matchResults = D1$Y[matches_smaller_iboth[c((n_matches)-4):n_matches]]
+    
+    matchesInPlayer0Column = match(matches_smaller_i0[c((n_matches0-4):n_matches0)[which(c((n_matches0-4):n_matches0)>0)]],
+                                   matches_smaller_iboth[c((n_matches)-4):n_matches])
+    matchesInPlayer0ColumnNoNA = matchesInPlayer0Column[which(!is.na(matchesInPlayer0Column))]
+    
+    matchesInPlayer1Column = match(matches_smaller_i1[c((n_matches1-4):n_matches1)[which(c((n_matches1-4):n_matches1)>0)]],
+                                   matches_smaller_iboth[c((n_matches)-4):n_matches])
+    matchesInPlayer1ColumnNoNA = matchesInPlayer1Column[which(!is.na(matchesInPlayer1Column))]
+    
+    D1$form_5_player0[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
+  } else {
+    D1$form_5_player0[i] = NA
+  }
+  
+  # Calculating form for player1
+  matches0 = which(as.character(D1$player0) == as.character(D1$player1[i]))
+  matches1 = which(as.character(D1$player1) == as.character(D1$player1[i]))
+  matches_smaller_i_indeces0 = which(matches0<i)
+  matches_smaller_i_indeces1 = which(matches1<i)
+  matches_smaller_i0 = matches0[matches_smaller_i_indeces0]
+  matches_smaller_i1 = matches1[matches_smaller_i_indeces1]
+  matches_smaller_iboth = sort(append(matches_smaller_i1, matches_smaller_i0))
+  
+  n_matches0 = length(matches_smaller_i0)
+  n_matches1 = length(matches_smaller_i1)
+  n_matches = length(matches_smaller_iboth)
+  if (n_matches >= 10) {
+    matchResults = D1$Y[matches_smaller_iboth[c((n_matches)-4):n_matches]]
+    
+    matchesInPlayer0Column = match(matches_smaller_i0[c((n_matches0-4):n_matches0)[which(c((n_matches0-4):n_matches0)>0)]],
+                                   matches_smaller_iboth[c((n_matches)-4):n_matches])
+    matchesInPlayer0ColumnNoNA = matchesInPlayer0Column[which(!is.na(matchesInPlayer0Column))]
+    
+    matchesInPlayer1Column = match(matches_smaller_i1[c((n_matches1-4):n_matches1)[which(c((n_matches1-4):n_matches1)>0)]],
+                                   matches_smaller_iboth[c((n_matches)-4):n_matches])
+    matchesInPlayer1ColumnNoNA = matchesInPlayer1Column[which(!is.na(matchesInPlayer1Column))]
+    
+    D1$form_5_player1[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
+  } else {
+    D1$form_5_player1[i] = NA
+  }
+}
+
+
+# Creating form feature for last 10 games ---------------------------------------------------
+
+D1 = D1 %>%
+  mutate(form_1_player0 = NA) %>%
+  mutate(form_1_player1 = NA)
+
+for (i in 1:nrow(D1)) {
+  # Calculating Form for player0
+  matches0 = which(as.character(D1$player0) == as.character(D1$player0[i]))
+  matches1 = which(as.character(D1$player1) == as.character(D1$player0[i]))
+  matches_smaller_i_indeces0 = which(matches0<i)
+  matches_smaller_i_indeces1 = which(matches1<i)
+  matches_smaller_i0 = matches0[matches_smaller_i_indeces0]
+  matches_smaller_i1 = matches1[matches_smaller_i_indeces1]
+  matches_smaller_iboth = sort(append(matches_smaller_i1, matches_smaller_i0))
+  
+  n_matches0 = length(matches_smaller_i0)
+  n_matches1 = length(matches_smaller_i1)
+  n_matches = length(matches_smaller_iboth)
+  if (n_matches >= 5) {
+    matchResults = D1$Y[matches_smaller_iboth[n_matches]]
+    
+    matchesInPlayer0Column = match(matches_smaller_i0[n_matches0[which(n_matches0>0)]],
+                                   matches_smaller_iboth[n_matches])
+    matchesInPlayer0ColumnNoNA = matchesInPlayer0Column[which(!is.na(matchesInPlayer0Column))]
+    
+    matchesInPlayer1Column = match(matches_smaller_i1[n_matches1[which(n_matches1>0)]],
+                                   matches_smaller_iboth[n_matches])
+    matchesInPlayer1ColumnNoNA = matchesInPlayer1Column[which(!is.na(matchesInPlayer1Column))]
+    
+    D1$form_1_player0[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
+  } else {
+    D1$form_1_player0[i] = NA
+  }
+  
+  # Calculating form for player1
+  matches0 = which(as.character(D1$player0) == as.character(D1$player1[i]))
+  matches1 = which(as.character(D1$player1) == as.character(D1$player1[i]))
+  matches_smaller_i_indeces0 = which(matches0<i)
+  matches_smaller_i_indeces1 = which(matches1<i)
+  matches_smaller_i0 = matches0[matches_smaller_i_indeces0]
+  matches_smaller_i1 = matches1[matches_smaller_i_indeces1]
+  matches_smaller_iboth = sort(append(matches_smaller_i1, matches_smaller_i0))
+  
+  n_matches0 = length(matches_smaller_i0)
+  n_matches1 = length(matches_smaller_i1)
+  n_matches = length(matches_smaller_iboth)
+  if (n_matches >= 10) {
+    matchResults = D1$Y[matches_smaller_iboth[c((n_matches)-4):n_matches]]
+    
+    matchesInPlayer0Column = match(matches_smaller_i0[n_matches0[which(n_matches0>0)]],
+                                   matches_smaller_iboth[n_matches])
+    matchesInPlayer0ColumnNoNA = matchesInPlayer0Column[which(!is.na(matchesInPlayer0Column))]
+    
+    matchesInPlayer1Column = match(matches_smaller_i1[n_matches1[which(n_matches1>0)]],
+                                   matches_smaller_iboth[n_matches])
+    matchesInPlayer1ColumnNoNA = matchesInPlayer1Column[which(!is.na(matchesInPlayer1Column))]
+    
+    D1$form_1_player1[i] = length(which(matchResults[matchesInPlayer0ColumnNoNA] == 0)) + length(which(matchResults[matchesInPlayer1ColumnNoNA] == 1))
+  } else {
+    D1$form_1_player1[i] = NA
+  }
+}
+
+# Creating combined weighted form of last 10 games ---------------------------------------------------
+
+D1 = D1 %>%
+  mutate(form_weighted_player0 = D1$form_1_player0 + D1$form_5_player0 + D1$form_10_player0) %>%
+  mutate(form_weighted_player1 = D1$form_1_player1 + D1$form_5_player1 + D1$form_10_player1)
 
 
 # Saving -------------------------------------------------------------
 
 form = D1
-# save(form, file = "../Roeser, Jonas - 2_Data/form.RData")
+save(form, file = "../Roeser, Jonas - 2_Data/form.RData")
