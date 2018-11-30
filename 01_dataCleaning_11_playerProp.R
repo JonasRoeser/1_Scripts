8456eea0aa45619b2c2b51de063c4ea41cbf1b7e
leonidasbothmer <44315917+leonidasbothmer@users.noreply.github.com>
2018-11-30 16:00:42 +0100

    minor changes

--- 01_dataCleaning_11_playerProp.R
+++ 01_dataCleaning_11_playerProp.R
@@ -15,14 +15,17 @@ load("../2_Data/D1.RData")
 
 # Ceating Feature: Age Difference -----
 
+# Create column with the age difference of player 0 and player 1
 D1.prop = D1[c(2,29,47,35,53)] %>%
   mutate(age_diff = D1$player_age.x - D1$player_age.y) %>%
 
 
 # Ceating Feature: Height Difference -----
 
+  # Create column with the height difference player 0 and player 1
   mutate(height_diff = D1$height_cm.x - D1$height_cm.y)
 
+# Select relevant columns: Match_id
 D1.prop = D1.prop[c(1,6,7)]
 
 
