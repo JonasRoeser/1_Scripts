bada0b73a552c8f0520c3060a3a892a5e3576f52
JonasRoeser <jonas@schneider-roeser.de>
2018-12-01 16:58:49 +0100

    Update 02_dataAnalysis_01_GLM.R

--- 02_dataAnalysis_01_GLM.R
+++ 02_dataAnalysis_01_GLM.R
@@ -1,160 +1,203 @@
 
-# K-FOLD CROSS VALIDATION LOGISTIC REGRESSION
+# LOGISTIC REGRESSION
+
+# Setup -------------------------------------------------------------------
 
 library(tidyverse)
+library(caret)
+library(pROC)
 
 rm(list = ls())
 
 # Reading the previously saved version of our data
 load("../Roeser, Jonas - 2_Data/DF.RData")
 
-
 # Because of OneDrive we need to load from two different paths
 load("../2_Data/DF.RData")
 
-# U1 = na.omit(U1)
-# U1 = as.matrix(U1)
+
+# Preperation -------------------------------------------------------------
+
+DFopt = DF[1:(0.3*nrow(DF)),]
+  
+DFkfold = DF[-(1:(0.3*nrow(DF))),]
 
 
-kfold_logit = function(comb) {
-  U1 = DF[,c(comb,11)]
+# Optimisation ------------------------------------------------------------
+
+# Pre-optimising hyperparamters with all features -------------------------
+
+# Creating function that takes feature combination vector as input
+logistic = function(comb) {
+  DFtrain = DFopt[1:(0.7*nrow(DFopt)),comb]
+  Xtrain = as.matrix(DFtrain[,c(1:(ncol(DFtrain)-1))])
+  Ytrain <<- as.matrix(DFtrain[,ncol(DFtrain)])
   
-  errors = matrix(0, 10, 6 )
-  colnames(errors) = c("f_pos_train", 
-                       "f_neg_train", 
-                       "acc_train", 
-                       "f_pos_test", 
-                       "f_neg_test", 
-                       "acc_test")
+  DFtest = DFopt[-(1:(0.7*nrow(DFopt))),comb]
+  Xtest = as.matrix(DFtest[,c(1:(ncol(DFtest)-1))])
+  Ytest <<- as.matrix(DFtest[,ncol(DFtest)])
   
-  set.seed(1)
-  # Randomly shuffle the data
-  n = nrow(U1)
-  # n = 1000
-  U1 = U1[sample(n),]
-  # Create 10 equally size folds
-  folds = cut(seq(1,nrow(U1)),breaks=10,labels=FALSE)
-  # Perform 10 fold cross validation
-  for(i in 1:10){
-    # Segement your data by fold using the which() function 
-    testIndexes <- which(folds==i,arr.ind=TRUE)
-    testData <- U1[testIndexes, ]
-    trainData <- U1[-testIndexes, ]
-    
-    # Splitting X and Y
-    Xtrain = trainData[,c(1:(ncol(U1)-1))]
-    Ytrain = trainData[,ncol(U1)]
-    Xtest = testData[,c(1:(ncol(U1)-1))]
-    Ytest = testData[,ncol(U1)]
-    
-    # Binding X and Y
-    data <- data.frame(Ytrain, Xtrain)
-    
-    # Applying logistic regresion
-    model <<- glm(Ytrain ~ ., data, family=binomial(link='logit'))
-    
-    # Extracting the betas
-    beta_logistic  = model$coefficients
-    
-    # Calculating the Etas
-    eta_log_train = rep(0, nrow(Xtrain))
-    prob_log_train = rep(0, nrow(Xtrain))
-    for (j in 1:nrow(Xtrain)) {
-      eta_log_train[j] = round(exp(c(1, Xtrain[j,]) %*% beta_logistic) / (1+exp(c(1,Xtrain[j,]) %*% beta_logistic)))
-      prob_log_train[j] = exp(c(1, Xtrain[j,]) %*% beta_logistic) / (1+exp(c(1,Xtrain[j,]) %*% beta_logistic))
-    }
-    
-    # Calculating Training Errors ---------------------------------------------
-    
-    
-    for (j in 1:nrow(Xtrain)) {
-      if(eta_log_train[j] == 1 && Ytrain[j] == 0) {
-        errors[[i,1]] = errors[[i,1]] + 1
-      }
-      if(eta_log_train[j] == 0 && Ytrain[j] == 1) {
-        errors[[i,2]] = errors[[i,2]] + 1
-      }
-    }
-    
-    errors[[i,1]] = errors[[i,1]]/nrow(Xtrain) # Calculating a percentage
-    errors[[i,2]] = errors[[i,2]]/nrow(Xtrain) # Calculating a percentage
-    errors[[i,3]] = 1-(errors[[i,1]] + errors[[i,2]]) # Calculating the accuracy
-    
-    # Testing -----------------------------------------------------------------
-    
-    eta_log_test = rep(0, nrow(Xtest))
-    prob_log_test = rep(0, nrow(Xtest))
-    for (j in 1:nrow(Xtest)) {
-      eta_log_test[j] = round(exp(c(1, Xtest[j,]) %*% beta_logistic) / (1+exp(c(1,Xtest[j,]) %*% beta_logistic)))
-      prob_log_test[j] = exp(c(1, Xtrain[j,]) %*% beta_logistic) / (1+exp(c(1,Xtrain[j,]) %*% beta_logistic))
-    }
-    
-    # Calculating Testing Errors ----------------------------------------------
-    
-    for (j in 1:nrow(Xtest)) {
-      if(eta_log_test[j] == 1 && Ytest[j] == 0) {
-        errors[[i,4]] = errors[[i,4]] + 1
-      }
-      if(eta_log_test[j] == 0 && Ytest[j] == 1) {
-        errors[[i,5]] = errors[[i,5]] + 1
-      }
-    }
-    
-    errors[[i,4]] = errors[[i,4]]/nrow(Xtest) # Calculating a percentage
-    errors[[i,5]] = errors[[i,5]]/nrow(Xtest) # Calculating a percentage
-    errors[[i,6]] = 1-(errors[[i,4]] + errors[[i,5]]) # Calculating the accuracy
-    
+  # Applying logistic regresion
+  model = glm(Y ~ ., data=DFtrain, family=binomial(link='logit'))
+  
+  # Extracting the betas
+  beta_logistic  = model$coefficients
+  
+  # Calculating training etas & probabilities
+  eta_log_train <<- rep(0, nrow(Xtrain))
+  prob_log_train <<- rep(0, nrow(Xtrain))
+  for (i in 1:nrow(Xtrain)) {
+    eta_log_train[i] <<- round(exp(c(1, Xtrain[i,]) %*% beta_logistic) / (1+exp(c(1,Xtrain[i,]) %*% beta_logistic)))
+    prob_log_train[i] <<- exp(c(1, Xtrain[i,]) %*% beta_logistic) / (1+exp(c(1,Xtrain[i,]) %*% beta_logistic))
   }
   
-  return(model_acc_log = t(colMeans(errors)))
+  # Calculating training accuracy
+  errors = 0
+  for (i in 1:nrow(Xtrain)) {
+    if(eta_log_train[i] != Ytrain[i]) {
+      errors = errors + 1
+    }
+  }
+  accuracy_train = 1-(errors/nrow(Xtrain))
+  
+  # Calculating testing etas & probabilities
+  eta_log_test <<- rep(0, nrow(Xtest))
+  prob_log_test <<- rep(0, nrow(Xtest))
+  for (i in 1:nrow(Xtest)) {
+    eta_log_test[i] <<- round(exp(c(1, Xtest[i,]) %*% beta_logistic) / (1+exp(c(1,Xtest[i,]) %*% beta_logistic)))
+    prob_log_test[i] <<- exp(c(1, Xtest[i,]) %*% beta_logistic) / (1+exp(c(1,Xtest[i,]) %*% beta_logistic))
+  }
+  
+  # Calculating testing errors
+  errors = 0
+  for (i in 1:nrow(Xtest)) {
+    if(eta_log_test[i] != Ytest[i]) {
+      errors = errors + 1
+    }
+  }
+  accuracy_test = 1-(errors/nrow(Xtest))
+  
+  return(c(accuracy_train, accuracy_test))
 }
 
-feature_combinations = matrix(nrow = 31, ncol = 7)
+# As there are no hyperparameters to optimise for logistic regression, the optimal model with all features is:
+logistic(c(1,10))
 
-# This for loop takes quite some time to finish
-# k = 1
-# for(i in 1:5) {
-#   combinations = rbind(combn(1:5, i, FUN = NULL, simplify = TRUE), combn(1:5, i, FUN = NULL, simplify = TRUE)+5)
-#   for(j in 1:ncol(combinations)) {
-#     feature_combinations[k, 1] = paste(combinations[,j], collapse=",")
-#     feature_combinations[k, 2:7] = kfold_logit(combinations[,j])
+
+# Optimising features with pre-optimised hyperparameters ------------------
+
+feature_test = matrix(nrow = 511, ncol = 3)
+colnames(feature_test) = c("comb",
+                           "accuracy_train",
+                           "accuracy_test")
+
+# This for-loop takes quite long to finish
+# k = 0
+# for(i in 1:9) {
+#   for(j in 1:ncol(combn(9,i))) {
 #     k = k + 1
+#     feature_test[k,1] = paste(rbind(combn(9,i))[,j], collapse = ",")
+#     feature_test[k,2:3] = logistic(c(as.double(paste(rbind(combn(9,i))[,j])),10))
 #   }
 # }
+# save(feature_test, file = "../Roeser, Jonas - 2_Data/feature_test.RData")
+
+# Reading the previously saved version of our data
+load("../Roeser, Jonas - 2_Data/feature_test.RData")
+
+# Because of OneDrive we need to load from two different paths
+load("../2_Data/feature_test.RData")
+
+best_comb = feature_test[which.max(feature_test[,3]),1]
+# --> We get the highest testing accuracy training with all feateures, except head to head!
 
 
+# Optimising hyperparamters with optimised features -----------------------
 
-kfold_logit(c(1,2,3,4,5,6,7,8,9,10))
-
-# write.csv(feature_combinations, file = "../Roeser, Jonas - 2_Data/feature_combinations.csv")
-
-# significance of features
-# install.packages("caret")
-library(caret)
-
-summary(model)
-
-varImp(model, scale = FALSE)
-
-prob = predict(model,type=c("response"))
+# Again, there are no hyperparameters to optimise for logistic regression
 
 
-# Plotting ----------------------------------------------------------------
+# Plotting ROC curve ------------------------------------------------------
 
-# plot(Xtest[,1], Xtest[,2])
-# points(Xtrain)
-# x_line = seq(1, 2000)
-# y_line = (-beta_logistic[1] - beta_logistic[2] * x_line) / beta_logistic[3]
-# lines(x_line, y_line, col="blue", lwd=2)
+best_comb
 
+logistic(c(1,2,3,4,5,7,8,9,10))
 
-
-# plotting ROC
-# install.packages("pROC")
-library(pROC)
 roc_train = roc(Ytrain ~ prob_log_train,
-                 auc = T)
+                auc = T)
 plot(roc_train)
 roc_test = roc(Ytest ~ prob_log_test,
-                auc = T)
+               auc = T)
 plot(roc_test)
+
+
+# Kfold -------------------------------------------------------------------
+
+logistic_kfold = function(comb) {
+  DFtrain = DFkfold[testIndexes,comb]
+  Xtrain = as.matrix(DFtrain[,c(1:(ncol(DFtrain)-1))])
+  Ytrain = as.matrix(DFtrain[,ncol(DFtrain)])
+  
+  DFtest = DFkfold[-testIndexes,comb]
+  Xtest = as.matrix(DFtest[,c(1:(ncol(DFtest)-1))])
+  Ytest = as.matrix(DFtest[,ncol(DFtest)])
+  
+  # Applying logistic regresion
+  model = glm(Y ~ ., data=DFtrain, family=binomial(link='logit'))
+  
+  # Extracting the betas
+  beta_logistic  = model$coefficients
+  
+  # Calculating training etas & probabilities
+  eta_log_train = rep(0, nrow(Xtrain))
+  prob_log_train = rep(0, nrow(Xtrain))
+  for (i in 1:nrow(Xtrain)) {
+    eta_log_train[i] = round(exp(c(1, Xtrain[i,]) %*% beta_logistic) / (1+exp(c(1,Xtrain[i,]) %*% beta_logistic)))
+    prob_log_train[i] = exp(c(1, Xtrain[i,]) %*% beta_logistic) / (1+exp(c(1,Xtrain[i,]) %*% beta_logistic))
+  }
+  
+  # Calculating training accuracy
+  errors = 0
+  for (i in 1:nrow(Xtrain)) {
+    if(eta_log_train[i] != Ytrain[i]) {
+      errors = errors + 1
+    }
+  }
+  accuracy_train = 1-(errors/nrow(Xtrain))
+  
+  # Calculating testing etas & probabilities
+  eta_log_test = rep(0, nrow(Xtest))
+  prob_log_test = rep(0, nrow(Xtest))
+  for (i in 1:nrow(Xtest)) {
+    eta_log_test[i] = round(exp(c(1, Xtest[i,]) %*% beta_logistic) / (1+exp(c(1,Xtest[i,]) %*% beta_logistic)))
+    prob_log_test[i] = exp(c(1, Xtest[i,]) %*% beta_logistic) / (1+exp(c(1,Xtest[i,]) %*% beta_logistic))
+  }
+  
+  # Calculating testing errors
+  errors = 0
+  for (i in 1:nrow(Xtest)) {
+    if(eta_log_test[i] != Ytest[i]) {
+      errors = errors + 1
+    }
+  }
+  accuracy_test = 1-(errors/nrow(Xtest))
+  
+  return(c(accuracy_train, accuracy_test))
+}
+
+best_comb
+
+# Create 10 equally sized folds
+folds = cut(seq(1,nrow(DFkfold)),breaks=10,labels=FALSE)
+
+# Create kfold matrix
+kfold = matrix(nrow = 10, ncol = 2)
+
+# Perform 10 fold cross validation
+for(i in 1:10){
+  # Segement your data by fold using the which() function
+  testIndexes = which(folds==i,arr.ind=TRUE)
+  kfold[i,] = logistic_kfold(c(1,2,3,4,5,7,8,9,10))
+}
+
+colMeans(kfold)
