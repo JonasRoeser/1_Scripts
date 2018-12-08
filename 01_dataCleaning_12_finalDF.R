e4c884c5a6f4e1ad7087a6e5bb2943c607f322c3
CSchramm <cornelius.schramm@student.unisg.ch>
2018-12-08 13:39:35 +0100

    DFref

--- 01_dataCleaning_12_finalDF.R
+++ 01_dataCleaning_12_finalDF.R
@@ -28,9 +28,15 @@ load("../2_Data/playerProp.RData")
 set.seed(1)
 
 
+
+
 # Selecting & renaming columns of D1 --------------------------------------------------------------
 
+# Creating DFref for reference (for presentation)
+# DFref = D1[,c(2,4,6,7,22,23,27,59,40,41,45,60,56)]
+
 D1 = D1[,c(27,59,45,60,56)]
+DFref = D1
 
 colnames(D1) = c("rank_number_p0",
                  "ranking_move_p0",
@@ -42,6 +48,9 @@ colnames(D1) = c("rank_number_p0",
 # Adding form, h2h, fatigue, homeGame & condition_wins ---------------
 
 D1 = cbind(D1, form[,c(6:13)], h2h[,c(11,12)], condition_wins[,c(8,9)], fatigue[,c(7,8)], homeGame[,c(5,6)], playerProp[,c(2:4)])
+DFref = cbind(DFref, form[,c(6:13)], h2h[,c(11,12)], condition_wins[,c(8,9)], fatigue[,c(7,8)], homeGame[,c(5,6)], playerProp[,c(2:4)])
+
+
 
 # Creating rank difference from rank_player_0 and rank_player_1
 D1 = D1 %>%
@@ -61,6 +70,9 @@ D1 = D1 %>%
 # Ordering and selecting relevant features
 D1 = D1[,c(22:25,29:33,5)]
 colnames(D1)[3] = "title_diff"
+
+# Prdering ad selectig DFref
+DFref = DFref[,c(2,4,6,7,15,16,20,22,23,27:35,59,61,63,65,67,69,71,73,75,38,40,41,45,53,60,62,64,66,68,70,72,74,76,56)]
 
 # separate Y from df
 Y = D1$Y
@@ -95,3 +107,4 @@ DF = sample_n(as.data.frame(D1),nrow(D1))
 
 # Saving "DF" as "DF.RData"
 # save(DF, file = "../Roeser, Jonas - 2_Data/DF.RData")
+# save(DFref, file = "../Roeser, Jonas - 2_Data/DFref.RData")
